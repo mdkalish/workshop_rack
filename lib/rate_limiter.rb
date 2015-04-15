39f1443756f280cf47669049027a6fd149af7bfe
mdkalish <mdkalish4git@gmail.com>
2015-04-15 14:49:40 +0200

    [#7] Fixup: improve code ligibility, reduce overhead

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -1,54 +1,60 @@
 require 'time'
 require 'rate_limiter/store'
+require 'pry'
 
 class RateLimiter
   def initialize(app, options = {}, &block)
     @app = app
     @options = options
-    @remaining_requests = @options[:limit] || 60
+    @default_requests_limit = @options[:limit] || 60
     @clients = options[:store] || Store.new
-    @block = block
+    @block = block || ->(args) { args["HTTP_X_FORWARDED_FOR"] || args['REMOTE_ADDR'] }
   end
 
   def call(env)
-    @block = ->(args) { args["HTTP_X_FORWARDED_FOR"] || args['REMOTE_ADDR'] } if @block.nil?
     return @app.call(env) if @block.call(env).nil?
     @id = @block.call(env)
-    set_client_limit if @clients.get(@id).nil?
-    return [429, {}, ['Too many requests.']] if @clients.get(@id)['remaining_requests'] <= 0
-    @status, @headers, body = @app.call(env)
-    update_headers_values
+    set_remaining_requests if @clients.get(@id).nil?
+    @clients_limits = @clients.get(@id)
+    reset_clients_limits if should_reset_limits?
+    return too_many_requests if request_limit_exceeded?
+    status, @headers, body = @app.call(env)
+    decrease_ratelimit
+    store_updated_limits
     set_headers
-    [@status, @headers, body]
+    [status, @headers, body]
   end
 
   private
 
-  def set_client_limit
-    @clients.set(@id, 'remaining_requests' => @remaining_requests + 1)
+  def set_remaining_requests
+    @clients.set(@id, 'remaining_requests' => @default_requests_limit)
   end
 
-  def update_headers_values
-    reset_time
-    decrease_ratelimit
+  def too_many_requests
+    [429, {}, ['Too many requests.']]
   end
 
-  def reset_time
-    @reset_time = @clients.get(@id)['reset_time']
-    if @reset_time.nil? || Time.now.to_i - @reset_time > 3600
-      @reset_time = Time.now.to_i
-      @remaining_requests = @options[:limit] || 60
-      store_updated_values
-    end
+  def request_limit_exceeded?
+    @clients_limits['remaining_requests'] <= 0
+  end
+
+  def reset_clients_limits
+    @reset_time = Time.now.to_i
+    @remaining_requests = @default_requests_limit
+    store_updated_limits
+  end
+
+  def should_reset_limits?
+    @clients_limits['reset_time'].nil? || Time.now.to_i - @clients_limits['reset_time'] > 3600
   end
 
   def decrease_ratelimit
     @remaining_requests = @clients.get(@id)['remaining_requests'] -= 1
-    store_updated_values
   end
 
   def set_headers
-    set_header('X-RateLimit-Limit', @options[:limit] || 60)
+    set_header('X-RateLimit-Limit', @default_requests_limit)
     set_header('X-RateLimit-Remaining', @remaining_requests)
     set_header('X-RateLimit-Reset', @reset_time)
   end
@@ -57,7 +63,7 @@ class RateLimiter
     @headers[header] = value.to_s
   end
 
-  def store_updated_values
+  def store_updated_limits
     @clients.set(@id, {'remaining_requests' => @remaining_requests,
                        'reset_time' => @reset_time})
   end
